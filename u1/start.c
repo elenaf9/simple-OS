c28716429d0ed6c146a1c0c372adf8d6c1e6ca81
Asphyxia <stefanvesovic2@gmail.com>
2022-11-05 22:41:11 +0100

    Added print_as_hex

--- u1/start.c
+++ u1/start.c
@@ -1,13 +1,13 @@
 #include "print.h"
 
-__attribute__((naked, section(".init")))
-void _start(void) {
-    init_dbgu();
-    int i = 0;
-    while (1) {
-        char val = read_char();
-        printf("[%x] %s: %c\n", i, "Received", val);
-        i++;
-    }
-    reset();
+__attribute__((naked, section(".init"))) void _start(void) {
+  init_dbgu();
+  int i = 0;
+
+  while (1) {
+    char val = read_char();
+    printf("[%x] %s: %c\n", i, "Received", val);
+    i++;
+  }
+  reset();
 }
