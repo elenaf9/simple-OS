c134ca02424ba3b60b21c51f95be76e52ec41bed
Elena Frank <elena.frank@protonmail.com>
2023-01-15 01:24:40 +0100

    refactor: use single stack per threads
    Only use a single stack (the user mode stack) per thread to store the
    context. Before, two stacks were being initialized and managed, namely
    one for the usr mode and one for the irq mode.

--- system/threads.c
+++ system/threads.c
@@ -87,7 +87,7 @@ static void init_tcb(unsigned int index, thread_fn thread_function,
 static void idle_thread(int p) {
   if (p != smoke_test_parameter) {
     // Something went wrong with the parameter.
-    printf("PANIC: wrong parameter!!!\n");
+    printf("PANIC: wrong parameter %x!!!\n", p);
     return;
   }
   while (1) {
@@ -184,7 +184,7 @@ void thread_switch(void) {
     runqueue->prev->next = runqueue;
   }
 
-  _switch_thread(prev_sp, &runqueue->data->sp);
+  *prev_sp = _switch_thread(runqueue->data->sp);
 }
 
 void change_thread_wait() {
