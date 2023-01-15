5dbb91027516389b816e0aa4590c970fa1071290
Elena Frank <elena.frank@protonmail.com>
2023-01-15 20:34:24 +0100

    feat: simplify thread management

--- include/processor.h
+++ include/processor.h
@@ -1,13 +1,10 @@
 #pragma once
 
-#include "threads.h"
-
 void _switch_cpu_mode(int);
 void _init_cpu_stack(int);
 
 void _cpu_disable_interrupt(void);
 void _cpu_enable_interrupt(void);
 
-unsigned int _init_thread_stack(int index, void (*run)(thread_fn, int), thread_fn,
-                          int);
-unsigned int _switch_thread(unsigned int);
+unsigned int _init_idle_thread_stack(int index, void (*)(void));
+unsigned int _switch_usr_stack(unsigned int);
--- include/threads.h
+++ include/threads.h
@@ -1,17 +1,8 @@
 #ifndef THREADS_H
 #define THREADS_H
 
-typedef void (*thread_fn)(int);
-
 void init_threading(void);
 int spawn_thread(unsigned int);
-void thread_switch(void);
-void change_thread_wait();
-
-void despawn_thread(int thread_id);
-void count_delay();
-
-void check_wait();
-void thread_wait();
+void switch_thread(void);
 
 #endif
