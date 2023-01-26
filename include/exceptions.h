b1a89f786b9c8db72d2a30fd2cb0e6ec906a75c4
Elena Frank <elena.frank@protonmail.com>
2023-01-26 12:21:18 +0100

    feat: print lr address on prefetched/ data abort

--- include/exceptions.h
+++ include/exceptions.h
@@ -1,8 +1,8 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
 
-void handle_data_abort(void);
-void handle_prefetched_abort(void);
+void handle_data_abort(int);
+void handle_prefetched_abort(int);
 void handle_software_interrupt(void);
 void handle_undefined_instruction(int);
 void handle_irq(void);
